c6e049e25e22e732d14261a9c715f4ef25c34bcd
Phil-DS <phillipdesousa9@gmail.com>
2021-12-11 12:07:42 +0000

    day 11

---
+++ day11/day11_2.py
@@ -0,0 +1,49 @@
+from day11_data import data
+from copy import deepcopy
+import numpy as np
+
+size = 10
+
+newState = deepcopy(data)
+days = 1000
+flashes = 0
+
+
+# for day in range(days):
+day = 1
+while True:
+    for i in range(size):
+        for j in range(size):
+            newState[j][i] += 1
+
+    flashed = set()
+    while True:
+        delayed = 0
+        for i in range(size):
+            for j in range(size):
+                if newState[j][i] >= 10 and (i,j) not in flashed:
+                    delayed += 1
+                    for di in range(-1,2):
+                        for dj in range(-1,2):
+                            
+                            if di == 0 and dj == 0:
+                                continue
+                            if (i+di) < 0 or (j+dj) < 0:
+                                continue
+                            try:
+                                newState[j+dj][i+di] += 1
+                            except:
+                                pass
+                    flashed.add((i,j))
+        
+        flashes += delayed
+        if delayed == 0:
+            break
+    for i,j in flashed:
+        newState[j][i] = 0
+    if np.sum(newState) == 0:
+        break
+    day += 1
+
+
+print(day)
