b086a7d1493dc5f9db2f672f84c76902033691bf
Phil-DS <phillipdesousa9@gmail.com>
2021-12-13 18:43:39 +0000

    Day 13

---
+++ day13/day13_2.py
@@ -0,0 +1,27 @@
+from numpy import sin
+from day13_data import data,folds
+# import numpy as np
+
+def fold(paper,axis,line):
+    rtn = set()
+    for x,y in paper:
+        if axis == 'x':
+            dist = abs(line - abs(line - x))
+            rtn.add((dist,y))
+        else:
+            dist = abs(line - abs(line - y))
+            rtn.add((x,dist))
+    
+    return rtn
+
+res = [*data]
+for folding in folds:
+    res = fold(res, *folding)
+
+# my size is 40/13
+scr = [[' ' for _ in range(50)] for _ in range(50)]
+for x,y in res:
+    scr[y][x] = '█'
+
+for row in scr:
+    print(*row,sep='')
