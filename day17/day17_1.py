2818f285f371bf6ae35010aac2e888d0c67900f8
Phil-DS <phillipdesousa9@gmail.com>
2021-12-30 22:18:45 +0000

    17 cleanup

--- day17/day17_1.py
+++ day17/day17_1.py
@@ -1,33 +1,22 @@
 from day17_data import *
 import math
 
-# s = ut + 1/2 at^2
-
-# we know that it decreases by 1 to hit the area. As such, we need to know the nearest triangle number to the  
-
-# We assume that its the positive
-# xMinLow = -0.5 + math.sqrt(0.5*0.5 - 4*0.5*xHigh)
-xMinLow = -0.5 + math.sqrt(0.5*0.5 - 4*0.5*(-xLow))
-
-# # We shall only use the ceil of the low. This gets us the 
-# t = math.ceil(tLow)
 def qf(a,b,c):
     disc = math.sqrt(b*b - 4*a*c)
     h = (-b + disc)/(2*a)
     l = (-b - disc)/(2*a)
-    return h,l
+    return [h,l]
 
 last = []
 for i in range(0, 100):
-    t = math.floor(max(list(qf(-0.5, i, -yLow))))
-    tHigh = math.floor(max(list(qf(-0.5, i, -yHigh))))
+    t = math.floor(max(qf(-0.5, i, -yLow)))
+    tHigh = math.floor(max(qf(-0.5, i, -yHigh)))
 
     s = (i*t - 0.5*t*t)
     if s <= yHigh:
-        tHigh = math.floor(max(list(qf(-0.5, i, -yHigh))))
+        tHigh = math.floor(max(qf(-0.5, i, -yHigh)))
 
         last.append(i)
-        print('Valid @ ',i)
 
 print(last[-1])
 
